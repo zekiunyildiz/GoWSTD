cd6eec3b4703bb102beff381b94a256f60d67442
zekiunyildiz <zekiunyildiz@gmail.com>
2021-09-14 23:50:16 +0300

    todoHandlers function w

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"log"
+	"net/http"
 	"time"
 
 	"github.com/go-chi/chi"
@@ -50,6 +51,31 @@ func main() {
 	r.Use(middleware.Logger)
 	r.Get("/", homeHandler)
 	r.Mount("/todo", todoHandlers())
+
+	srv := &http.Server{
+		Addr: port,
+		Handler: r,
+		ReadTimeout: 60*time.Second,
+		WriteTimeout: 60 * time.Second,
+		IdleTimeout: 60*time.Second,
+	}
+	go func ()  {
+		log.Println("Listening on port", port)
+		if err := srv.ListenAndServe(); err !=nil {
+			log.Println("Listen:%s\n", err)
+		}
+	}
+}
+
+func todoHandlers() http.Handler {
+	rg := chi.NewRouter()
+	rg.Group(func(r chi.Router) {
+		r.Get("/", fetchTodos)
+		r.Post("/", createTodo)
+		r.Put("/{id}", updateTodo)
+		r.Delete("/{id}", deleteTodo)
+	})
+	return rg
 }
 
 func checkErr(err error) {
