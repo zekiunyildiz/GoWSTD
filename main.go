2a016978c53efab7c93a45f52c174bc66bf957df
zekiunyildiz <zekiunyildiz@gmail.com>
2021-09-15 17:52:34 +0300

    fetchTodos function w

--- main.go
+++ main.go
@@ -54,6 +54,30 @@ func homeHandler(w http.ResponseWriter, r * http.Request)  {
 	checkErr(err)
 }
 
+func fetchTodos(w http.ResponseWriter, r * http.Request)  {
+	todos := []todoModel{}
+	if err := db.C(collectionName).Find(bson.M{}).All(&todos); err != nil{
+		rnd.JSON(w, http.StatusProcessing, renderer.M{
+			"Message":"Failed to fetch todo",
+			"error":err,
+		})
+		return
+	}
+	todoList := []todo{}
+
+	for _,t:=range todos{
+		todoList = append(todoList, todo{
+			ID: t.ID.Hex(),
+			Title: t.Title,
+			Completed: t.Completed,
+			CreatedAt: t.CreatedAt,
+		})
+	}
+	rnd.JSON(w, http.StatusOK, renderer.M{
+		"data":todoList,
+	})
+}
+
 func main() {
 	stopChan := make(chan os.Signal)
 	signal.Notify(stopChan, os.Interrupt)
